ec4d40f7f5e70c9912e8cd00f418d617e0c5210a
Okai Yeboah <kingkloy16@gmail.com>
2021-02-19 02:37:29 +0000

    finished app for the seminar

--- maf_00/lib/main.dart
+++ maf_00/lib/main.dart
@@ -5,43 +5,22 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -50,68 +29,238 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    double mScreenHeight = MediaQuery.of(context).size.height;
+    double mScreenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: Container(
+        height: mScreenHeight,
+        width: mScreenWidth,
+        child: Stack(
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            buildProfileImage(mScreenHeight, mScreenWidth),
+            Positioned(
+              top: mScreenHeight / 2.3,
+              child: Container(
+                height: mScreenHeight / 1.7,
+                width: mScreenWidth,
+                decoration: BoxDecoration(
+                  color: Color(0xeef5f4ff),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    spacerHeight20(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        buildUserName(),
+                        buildEditButton(),
+                      ],
+                    ),
+                    spacerHeight20(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        buildCategory("500", "Following"),
+                        buildCategory("20M", "Followers"),
+                        buildCategory("1.2k", "Posts"),
+                      ],
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(20),
+                      height: 200,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.3),
+                            spreadRadius: 3,
+                            blurRadius: 5,
+                            offset: Offset(0, 3), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        children: [
+                          spacerHeight20(),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              buildCategoryType(
+                                  imagePath: "assets/photos.png",
+                                  title: "Photos"),
+                              buildCategoryType(
+                                  imagePath: "assets/audio.png",
+                                  title: "Audio"),
+                              buildCategoryType(
+                                  imagePath: "assets/video.png",
+                                  title: "Video"),
+                            ],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 50.0, vertical: 20),
+                            child: Divider(
+                              height: 10,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              buildCategoryType(
+                                  imagePath: "assets/phone.png", title: "Call"),
+                              buildCategoryType(
+                                  imagePath: "assets/chat.png",
+                                  title: "Message"),
+                              buildCategoryType(
+                                  imagePath: "assets/activity.png",
+                                  title: "Activity"),
+                            ],
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: Container(
+        height: 70,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [Color(0xFf8643FF), Color(0xff4136F1)],
+          ),
+        ),
+        child: BottomNavigationBar(
+          elevation: 0,
+          currentIndex: 0,
+          backgroundColor: Colors.transparent,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                size: 30,
+                color: Color(0xFf8643FF),
+              ),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.dashboard_rounded,
+                size: 30,
+                color: Color(0xFf8643FF),
+              ),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.person,
+                  size: 30,
+                  color: Colors.white,
+                ),
+                label: '')
+          ],
+        ),
+      ),
+    );
+  }
+
+  Column buildCategoryType({String imagePath, String title}) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(5.0),
+          child: Image.asset(imagePath),
+        ),
+        Text(title)
+      ],
+    );
+  }
+
+  Column buildCategory(String value, String title) {
+    return Column(
+      children: [
+        Text(
+          "$value",
+          style: TextStyle(
+              color: Colors.black, fontWeight: FontWeight.bold, fontSize: 20),
+        ),
+        Text("$title")
+      ],
+    );
+  }
+
+  SizedBox spacerHeight20() => SizedBox(height: 20);
+
+  Widget buildEditButton() {
+    return GestureDetector(
+      onTap: () {
+        print("Flutter rocks");
+      },
+      child: Container(
+        height: 35,
+        width: 80,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [Color(0xFf8643FF), Color(0xff4136F1)])),
+        child: Center(
+          child: Text(
+            "Edit",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+
+  buildUserName() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Sedem Balfour",
+          style: TextStyle(
+              color: Colors.black, fontWeight: FontWeight.bold, fontSize: 20),
+        ),
+        Text("Accra, Ghana")
+      ],
+    );
+  }
+
+  Container buildProfileImage(double mScreenHeight, double mScreenWidth) {
+    return Container(
+      height: mScreenHeight,
+      width: mScreenWidth,
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [Colors.white, Colors.red[200]])),
+      child: Column(
+        children: [
+          Image.asset("assets/sedem.png"),
+        ],
+      ),
     );
   }
 }
